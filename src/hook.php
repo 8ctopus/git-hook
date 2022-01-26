e3646ca0450af98fb7b3f57ccaf508dea77befb6
8ctopus <hello@octopuslabs.io>
2022-01-26 11:40:30 +0500

    cleanup: linted code

--- src/hook.php
+++ src/hook.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Git pull script
  * @author 8ctopus
@@ -10,10 +12,10 @@
 define('_KEY', 'SECRET');
 
 // path to repository to pull
-define('_REPOPATH', $_SERVER['DOCUMENT_ROOT'].'/../');
+define('_REPOPATH', $_SERVER['DOCUMENT_ROOT'] . '/../');
 
 // path to logs
-define('_LOGPATH', $_SERVER['DOCUMENT_ROOT'].'/../logs/git-hook/');
+define('_LOGPATH', $_SERVER['DOCUMENT_ROOT'] . '/../logs/git-hook/');
 
 $log_base = 'git hook - ';
 
@@ -22,7 +24,7 @@ $section = @$_GET['section'];
 
 // check section
 if (empty($section)) {
-    error_log($log_base.' - FAILED - no section');
+    error_log($log_base . ' - FAILED - no section');
     header('HTTP/1.0 401 Unauthorized');
     exit();
 }
@@ -32,28 +34,28 @@ $log_base .= $section;
 
 switch ($section) {
     case 'site':
-        $path = _REPOPATH.'public_html';
+        $path = _REPOPATH . 'public_html';
         break;
 
     case 'store':
-        $path = _REPOPATH.'store';
+        $path = _REPOPATH . 'store';
         break;
 
     default:
-        error_log($log_base.' - FAILED - unknown section - '.$section);
+        error_log($log_base . ' - FAILED - unknown section - ' . $section);
         header('HTTP/1.0 401 Unauthorized');
         exit();
 }
 
 // check for POST request
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    error_log($log_base.' - FAILED - not POST - '.$_SERVER['REQUEST_METHOD']);
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    error_log($log_base . ' - FAILED - not POST - ' . $_SERVER['REQUEST_METHOD']);
     header('HTTP/1.0 401 Unauthorized');
     exit();
 }
 
 // get content type
-$content_type = isset($_SERVER['CONTENT_TYPE']) ? strtolower(trim($_SERVER['CONTENT_TYPE'])) : '';
+$content_type = isset($_SERVER['CONTENT_TYPE']) ? mb_strtolower(trim($_SERVER['CONTENT_TYPE'])) : '';
 
 switch ($content_type) {
     case 'application/json':
@@ -67,14 +69,14 @@ switch ($content_type) {
         break;
 
     default:
-        error_log($log_base.' - FAILED - unknown content type - '.$content_type);
+        error_log($log_base . ' - FAILED - unknown content type - ' . $content_type);
         header('HTTP/1.0 401 Unauthorized');
         exit();
 }
 
 // check payload exists
 if (empty($payload)) {
-    error_log($log_base.' - FAILED - no payload');
+    error_log($log_base . ' - FAILED - no payload');
     header('HTTP/1.0 401 Unauthorized');
     exit();
 }
@@ -83,7 +85,7 @@ if (empty($payload)) {
 $header_signature = isset($_SERVER['HTTP_X_GITEA_SIGNATURE']) ? $_SERVER['HTTP_X_GITEA_SIGNATURE'] : '';
 
 if (empty($header_signature)) {
-    error_log($log_base.' - FAILED - header signature missing');
+    error_log($log_base . ' - FAILED - header signature missing');
     header('HTTP/1.0 401 Unauthorized');
     exit();
 }
@@ -92,8 +94,8 @@ if (empty($header_signature)) {
 $payload_signature = hash_hmac('sha256', $payload, _KEY, false);
 
 // check payload signature against header signature
-if ($header_signature != $payload_signature) {
-    error_log($log_base.' - FAILED - payload signature');
+if ($header_signature !== $payload_signature) {
+    error_log($log_base . ' - FAILED - payload signature');
     header('HTTP/1.0 401 Unauthorized');
     exit();
 }
@@ -103,7 +105,7 @@ $decoded = json_decode($payload, true);
 
 // check for json decode errors
 if (json_last_error() !== JSON_ERROR_NONE) {
-    error_log($log_base.' - FAILED - json decode - '.json_last_error());
+    error_log($log_base . ' - FAILED - json decode - ' . json_last_error());
     //file_put_contents('/var/tmp/git-debug.log', var_export($payload, true));
     header('HTTP/1.0 401 Unauthorized');
     exit();
@@ -112,11 +114,11 @@ if (json_last_error() !== JSON_ERROR_NONE) {
 // prepare command
 $command = "cd $path;";
 
-if ($section == 'site') {
+if ($section === 'site') {
     // pull and run composer
     $command .= '/usr/bin/git pull 2>&1;';
     $command .= '/usr/bin/composer install --no-interaction --no-dev 2>&1;';
-} elseif ($section == 'store') {
+} elseif ($section === 'store') {
     // pull, run composer then php artisan
     // adjust to your needs
     $command .= '/usr/bin/git pull 2>&1;';
@@ -136,25 +138,25 @@ $dir = _LOGPATH;
 
 if (!file_exists($dir)) {
     if (!mkdir($dir)) {
-        error_log($log_base.' - FAILED - create dir');
+        error_log($log_base . ' - FAILED - create dir');
     }
 }
 
-if (!file_put_contents($dir.date('Ymd-H:i:s').'.log', $command."\n\n".print_r($output, true))) {
-    error_log($log_base.' - FAILED - save log');
+if (!file_put_contents($dir . date('Ymd-H:i:s') . '.log', $command . "\n\n" . print_r($output, true))) {
+    error_log($log_base . ' - FAILED - save log');
 }
 
 $output_str = '';
 
 foreach ($output as $str) {
-    $output_str .= $str."\n";
+    $output_str .= $str . "\n";
 }
 
 // check command return code
-if ($status != 0) {
+if ($status !== 0) {
     header('HTTP/1.0 409 Conflict');
-    error_log($log_base.' - FAILED - command return code - make sure server git remote -v contains password and git branch --set-upstream-to=origin/master master - '.$output_str);
+    error_log($log_base . ' - FAILED - command return code - make sure server git remote -v contains password and git branch --set-upstream-to=origin/master master - ' . $output_str);
     exit();
 }
 
-error_log($log_base.' - OK - '.$output_str);
+error_log($log_base . ' - OK - ' . $output_str);
