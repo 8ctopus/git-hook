e61e915a6d50ba9bd39e95a6ead03e044dd03f84
8ctopus <hello@octopuslabs.io>
2022-12-13 11:59:32 +0500

    cleanup: code

--- src/hook.php
+++ src/hook.php
@@ -1,12 +1,12 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * Git pull script
  * @author 8ctopus
  * @note When called the script automatically git pulls
  */
+
+declare(strict_types=1);
 
 // secret key
 define('_KEY', 'SECRET');
@@ -17,20 +17,20 @@ define('_REPOPATH', $_SERVER['DOCUMENT_ROOT'] . '/../');
 // path to logs
 define('_LOGPATH', $_SERVER['DOCUMENT_ROOT'] . '/../logs/git-hook/');
 
-$log_base = 'git hook - ';
+$logBase = 'git hook - ';
 
 // get section to pull
-$section = @$_GET['section'];
+$section = $_GET['section'] ?? '';
 
 // check section
 if (empty($section)) {
-    error_log($log_base . ' - FAILED - no section');
-    header('HTTP/1.0 401 Unauthorized');
+    error_log($logBase . ' - FAILED - no section');
+    http_response_code(401);
     exit();
 }
 
 // add section to env name
-$log_base .= $section;
+$logBase .= $section;
 
 switch ($section) {
     case 'site':
@@ -42,22 +42,22 @@ switch ($section) {
         break;
 
     default:
-        error_log($log_base . ' - FAILED - unknown section - ' . $section);
-        header('HTTP/1.0 401 Unauthorized');
+        error_log($logBase . ' - FAILED - unknown section - ' . $section);
+        http_response_code(401);
         exit();
 }
 
 // check for POST request
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    error_log($log_base . ' - FAILED - not POST - ' . $_SERVER['REQUEST_METHOD']);
-    header('HTTP/1.0 401 Unauthorized');
+    error_log($logBase . ' - FAILED - not POST - ' . $_SERVER['REQUEST_METHOD']);
+    http_response_code(401);
     exit();
 }
 
 // get content type
-$content_type = isset($_SERVER['CONTENT_TYPE']) ? mb_strtolower(trim($_SERVER['CONTENT_TYPE'])) : '';
+$contentType = isset($_SERVER['CONTENT_TYPE']) ? mb_strtolower(trim($_SERVER['CONTENT_TYPE'])) : '';
 
-switch ($content_type) {
+switch ($contentType) {
     case 'application/json':
         // get RAW post data
         $payload = trim(file_get_contents('php://input'));
@@ -65,38 +65,38 @@ switch ($content_type) {
 
     case '':
         // get payload
-        $payload = @$_POST['payload'];
+        $payload = $_POST['payload'] ?? '';
         break;
 
     default:
-        error_log($log_base . ' - FAILED - unknown content type - ' . $content_type);
-        header('HTTP/1.0 401 Unauthorized');
+        error_log($logBase . ' - FAILED - unknown content type - ' . $contentType);
+        http_response_code(401);
         exit();
 }
 
 // check payload exists
 if (empty($payload)) {
-    error_log($log_base . ' - FAILED - no payload');
-    header('HTTP/1.0 401 Unauthorized');
+    error_log($logBase . ' - FAILED - no payload');
+    http_response_code(401);
     exit();
 }
 
 // get header signature
-$header_signature = isset($_SERVER['HTTP_X_GITEA_SIGNATURE']) ? $_SERVER['HTTP_X_GITEA_SIGNATURE'] : '';
+$headerSignature = isset($_SERVER['HTTP_X_GITEA_SIGNATURE']) ? $_SERVER['HTTP_X_GITEA_SIGNATURE'] : '';
 
-if (empty($header_signature)) {
-    error_log($log_base . ' - FAILED - header signature missing');
-    header('HTTP/1.0 401 Unauthorized');
+if (empty($headerSignature)) {
+    error_log($logBase . ' - FAILED - header signature missing');
+    http_response_code(401);
     exit();
 }
 
 // calculate payload signature
-$payload_signature = hash_hmac('sha256', $payload, _KEY, false);
+$payloadSignature = hash_hmac('sha256', $payload, _KEY, false);
 
 // check payload signature against header signature
-if ($header_signature !== $payload_signature) {
-    error_log($log_base . ' - FAILED - payload signature');
-    header('HTTP/1.0 401 Unauthorized');
+if ($headerSignature !== $payloadSignature) {
+    error_log($logBase . ' - FAILED - payload signature');
+    http_response_code(401);
     exit();
 }
 
@@ -105,9 +105,9 @@ $decoded = json_decode($payload, true);
 
 // check for json decode errors
 if (json_last_error() !== JSON_ERROR_NONE) {
-    error_log($log_base . ' - FAILED - json decode - ' . json_last_error());
+    error_log($logBase . ' - FAILED - json decode - ' . json_last_error());
     //file_put_contents('/var/tmp/git-debug.log', var_export($payload, true));
-    header('HTTP/1.0 401 Unauthorized');
+    http_response_code(401);
     exit();
 }
 
@@ -138,25 +138,25 @@ $dir = _LOGPATH;
 
 if (!file_exists($dir)) {
     if (!mkdir($dir)) {
-        error_log($log_base . ' - FAILED - create dir');
+        error_log($logBase . ' - FAILED - create dir');
     }
 }
 
 if (!file_put_contents($dir . date('Ymd-H:i:s') . '.log', $command . "\n\n" . print_r($output, true))) {
-    error_log($log_base . ' - FAILED - save log');
+    error_log($logBase . ' - FAILED - save log');
 }
 
-$output_str = '';
+$outputStr = '';
 
 foreach ($output as $str) {
-    $output_str .= $str . "\n";
+    $outputStr .= $str . "\n";
 }
 
 // check command return code
 if ($status !== 0) {
     header('HTTP/1.0 409 Conflict');
-    error_log($log_base . ' - FAILED - command return code - make sure server git remote -v contains password and git branch --set-upstream-to=origin/master master - ' . $output_str);
+    error_log($logBase . ' - FAILED - command return code - make sure server git remote -v contains password and git branch --set-upstream-to=origin/master master - ' . $outputStr);
     exit();
 }
 
-error_log($log_base . ' - OK - ' . $output_str);
+error_log($logBase . ' - OK - ' . $outputStr);
