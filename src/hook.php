b0b8ce96adf7140cbccdaad7977e84ac646fbb68
8ctopus <hello@octopuslabs.io>
2023-05-05 09:38:23 +0400

    Convert code to class

--- src/hook.php
+++ src/hook.php
@@ -17,146 +17,156 @@ define('_REPO_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../');
 // path to logs
 define('_LOG_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../logs/git-hook/');
 
-$logBase = 'git hook - ';
+class Hook
+{
+    public function __construct()
+    {
+    }
 
-// get section to pull
-$section = $_GET['section'] ?? '';
+    public function run() : void
+    {
+        $logBase = 'git hook - ';
 
-// check section
-if (empty($section)) {
-    error_log("{$logBase} - FAILED - no section");
-    http_response_code(401);
-    exit;
-}
+        // get section to pull
+        $section = $_GET['section'] ?? '';
 
-// add section to env name
-$logBase .= $section;
+        // check section
+        if (empty($section)) {
+            error_log("{$logBase} - FAILED - no section");
+            http_response_code(401);
+            exit;
+        }
 
-switch ($section) {
-    case 'site':
-        $path = _REPO_PATH . 'public_html';
-        break;
+        // add section to env name
+        $logBase .= $section;
 
-    case 'store':
-        $path = _REPO_PATH . 'store';
-        break;
+        switch ($section) {
+            case 'site':
+                $path = _REPO_PATH . 'public_html';
+                break;
 
-    default:
-        error_log("{$logBase} - FAILED - unknown section - {$section}");
-        http_response_code(401);
-        exit;
-}
+            case 'store':
+                $path = _REPO_PATH . 'store';
+                break;
 
-// check for POST request
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    error_log("{$logBase} - FAILED - not POST - {$_SERVER['REQUEST_METHOD']}");
-    http_response_code(401);
-    exit;
-}
+            default:
+                error_log("{$logBase} - FAILED - unknown section - {$section}");
+                http_response_code(401);
+                exit;
+        }
 
-// get content type
-$contentType = mb_strtolower(trim($_SERVER['CONTENT_TYPE'] ?? ''));
+        // check for POST request
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            error_log("{$logBase} - FAILED - not POST - {$_SERVER['REQUEST_METHOD']}");
+            http_response_code(401);
+            exit;
+        }
 
-switch ($contentType) {
-    case 'application/json':
-        // get RAW post data
-        $payload = trim(file_get_contents('php://input'));
-        break;
+        // get content type
+        $contentType = mb_strtolower(trim($_SERVER['CONTENT_TYPE'] ?? ''));
 
-    case '':
-        // get payload
-        $payload = $_POST['payload'] ?? '';
-        break;
+        switch ($contentType) {
+            case 'application/json':
+                // get RAW post data
+                $payload = trim(file_get_contents('php://input'));
+                break;
 
-    default:
-        error_log("{$logBase} - FAILED - unknown content type - {$contentType}");
-        http_response_code(401);
-        exit;
-}
+            case '':
+                // get payload
+                $payload = $_POST['payload'] ?? '';
+                break;
 
-// check payload exists
-if (empty($payload)) {
-    error_log("{$logBase} - FAILED - no payload");
-    http_response_code(401);
-    exit;
-}
+            default:
+                error_log("{$logBase} - FAILED - unknown content type - {$contentType}");
+                http_response_code(401);
+                exit;
+        }
 
-// get header signature
-$headerSignature = $_SERVER['HTTP_X_GITEA_SIGNATURE'] ?? '';
+        // check payload exists
+        if (empty($payload)) {
+            error_log("{$logBase} - FAILED - no payload");
+            http_response_code(401);
+            exit;
+        }
 
-if (empty($headerSignature)) {
-    error_log("{$logBase} - FAILED - header signature missing");
-    http_response_code(401);
-    exit;
-}
+        // get header signature
+        $headerSignature = $_SERVER['HTTP_X_GITEA_SIGNATURE'] ?? '';
 
-// calculate payload signature
-$payloadSignature = hash_hmac('sha256', $payload, _KEY, false);
+        if (empty($headerSignature)) {
+            error_log("{$logBase} - FAILED - header signature missing");
+            http_response_code(401);
+            exit;
+        }
 
-// check payload signature against header signature
-if ($headerSignature !== $payloadSignature) {
-    error_log("{$logBase} - FAILED - payload signature");
-    http_response_code(401);
-    exit;
-}
+        // calculate payload signature
+        $payloadSignature = hash_hmac('sha256', $payload, _KEY, false);
 
-// convert json to array
-$decoded = json_decode($payload, true);
+        // check payload signature against header signature
+        if ($headerSignature !== $payloadSignature) {
+            error_log("{$logBase} - FAILED - payload signature");
+            http_response_code(401);
+            exit;
+        }
 
-// check for json decode errors
-if (json_last_error() !== JSON_ERROR_NONE) {
-    error_log("{$logBase} - FAILED - json decode - " . json_last_error());
-    //file_put_contents('/var/tmp/git-debug.log', var_export($payload, true));
-    http_response_code(401);
-    exit;
-}
+        // convert json to array
+        $decoded = json_decode($payload, true);
 
-// prepare command
-$command = "cd {$path};";
+        // check for json decode errors
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            error_log("{$logBase} - FAILED - json decode - " . json_last_error());
+            //file_put_contents('/var/tmp/git-debug.log', var_export($payload, true));
+            http_response_code(401);
+            exit;
+        }
 
-if ($section === 'site') {
-    // pull and run composer
-    $command .= '/usr/bin/git pull 2>&1;';
-    $command .= '/usr/bin/composer install --no-interaction --no-dev 2>&1;';
-} elseif ($section === 'store') {
-    // pull, run composer then php artisan
-    // adjust to your needs
-    $command .= '/usr/bin/git pull 2>&1;';
-    $command .= '/usr/bin/composer install --no-interaction --no-dev 2>&1;';
-    $command .= 'php artisan optimize:clear 2>&1;';
-    $command .= 'php artisan migrate --force 2>&1;';
-} else {
-    // just pull
-    $command .= '/usr/bin/git pull;';
-}
+        // prepare command
+        $command = "cd {$path};";
 
-// execute commands
-exec($command, $output, $status);
+        if ($section === 'site') {
+            // pull and run composer
+            $command .= '/usr/bin/git pull 2>&1;';
+            $command .= '/usr/bin/composer install --no-interaction --no-dev 2>&1;';
+        } elseif ($section === 'store') {
+            // pull, run composer then php artisan
+            // adjust to your needs
+            $command .= '/usr/bin/git pull 2>&1;';
+            $command .= '/usr/bin/composer install --no-interaction --no-dev 2>&1;';
+            $command .= 'php artisan optimize:clear 2>&1;';
+            $command .= 'php artisan migrate --force 2>&1;';
+        } else {
+            // just pull
+            $command .= '/usr/bin/git pull;';
+        }
 
-// save log
-$dir = _LOG_PATH;
+        // execute commands
+        exec($command, $output, $status);
 
-if (!file_exists($dir)) {
-    if (!mkdir($dir)) {
-        error_log("{$logBase} - FAILED - create dir");
+        // save log
+        $dir = _LOG_PATH;
+
+        if (!file_exists($dir)) {
+            if (!mkdir($dir)) {
+                error_log("{$logBase} - FAILED - create dir");
+            }
+        }
+
+        if (!file_put_contents($dir . date('Ymd-H:i:s') . '.log', $command . "\n\n" . print_r($output, true))) {
+            error_log("{$logBase} - FAILED - save log");
+        }
+
+        $outputStr = '';
+
+        foreach ($output as $str) {
+            $outputStr .= $str . "\n";
+        }
+
+        // check command return code
+        if ($status !== 0) {
+            http_response_code(409);
+            error_log("{$logBase} - FAILED - command return code - make sure server git remote -v contains password and git branch --set-upstream-to=origin/master master - {$outputStr}");
+            exit;
+        }
+
+        error_log("{$logBase} - OK - {$outputStr}");
     }
 }
-
-if (!file_put_contents($dir . date('Ymd-H:i:s') . '.log', $command . "\n\n" . print_r($output, true))) {
-    error_log("{$logBase} - FAILED - save log");
-}
-
-$outputStr = '';
-
-foreach ($output as $str) {
-    $outputStr .= $str . "\n";
-}
-
-// check command return code
-if ($status !== 0) {
-    http_response_code(409);
-    error_log("{$logBase} - FAILED - command return code - make sure server git remote -v contains password and git branch --set-upstream-to=origin/master master - {$outputStr}");
-    exit;
-}
-
-error_log("{$logBase} - OK - {$outputStr}");
