958e10ce572924d61227909b69cba957543b7bda
8ctopus <hello@octopuslabs.io>
2023-05-05 09:36:08 +0400

    Add phpunit testing base

---
+++ tests/HookTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+final class HookTest extends TestCase
+{
+    public function testNothing() : void
+    {
+        static::assertTrue(true);
+    }
+}
