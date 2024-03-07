6d95ce5e2822df9989a23e4c091153506c5066b0
8ctopus <hello@octopuslabs.io>
2024-03-07 10:24:27 +0400

    Cleanup code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,23 +10,6 @@ use Psr\Http\Message\ServerRequestInterface;
 
 abstract class TestCase extends BaseTestCase
 {
-    public function mockRequest(string $method, string $uri, array $getParams = [], array $postParams = []) : self
-    {
-        unset($_GET, $_POST, $_FILES, $_SERVER);
-
-        $_SERVER['HTTP_HOST'] = 'localhost';
-        $_SERVER['REQUEST_METHOD'] = $method;
-        $_SERVER['REQUEST_URI'] = $uri;
-        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-        $_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58';
-        $_SERVER['REQUEST_PROTOCOL'] = '1.1';
-
-        $_GET = $getParams;
-        $_POST = $postParams;
-
-        return $this;
-    }
-
     /**
      * Mock server request
      *
